5e008e0a7463aaa68b074c4a2318fecd38a8757c
Sandeep Vittala <applesandy33@gmail.com>
2022-02-04 19:17:33 -0500

    pushing hash stuff

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     User.findAll({
-        attributes: { exclude: ['password'] }
+        //attributes: { exclude: ['password'] }
     })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
@@ -48,7 +48,26 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+        
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword){
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });  
+});
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
